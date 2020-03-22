874e6e8de2948f603970dbc8b312760af8456ac8
Carl Tillotson <nimzoking@gmail.com>
2020-03-22 16:51:49 +0000

    ch3

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'users.apps.UsersConfig', 
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.CustomUser'
